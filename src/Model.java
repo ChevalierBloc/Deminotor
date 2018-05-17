2d390551e9faba0f6a01fca987a82cf696611de3
ChevalierBloc <lucas.laine@orange.fr>
2018-05-17 15:00:41 +0200

    Modification du main

--- src/Model.java
+++ src/Model.java
@@ -1,8 +1,13 @@
+import java.awt.image.BufferedImage;
+
 public class Model {
+
+    // 1 = mines
 
     private int nbcaseligne;
     private int nbcasecolonne;
     private int[][] tabGrille;
+    int nbMines ;
 
     public Model() {
 
@@ -28,5 +33,31 @@ public class Model {
 
     public int[][] getTabGrille() {
         return tabGrille;
+    }
+
+    public int getNbMines() {
+        return nbMines;
+    }
+
+    public void setNbMines(int nbMines) {
+        this.nbMines = nbMines;
+    }
+
+    public void placeMine(){
+        while(nbMines != 0){
+            for (int i = 0; i <nbcaseligne; i++) {
+                for (int j=0; i<nbcasecolonne ; j++){
+                    int alea = (int)(Math.random()*100) ;
+                    if (alea == 1 && nbMines > 0 && tabGrille[i][j] == 1){
+                        tabGrille[i][j] = 1 ;
+                        nbMines -=1 ;
+                    }
+                }
+
+            }
+        }
+    }
+    public void placeVoisins(){
+
     }
 }
