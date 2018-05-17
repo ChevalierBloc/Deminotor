3667e57dfa2199461bc2153e57696e2cd909a814
SamuelC <samuix21700@gmail.com>
2018-05-17 14:38:44 +0200

    Base d'appli et de model

--- src/Model.java
+++ src/Model.java
@@ -1,4 +1,32 @@
 public class Model {
+
+    private int nbcaseligne;
+    private int nbcasecolonne;
+    private int[][] tabGrille;
+
     public Model() {
+
+    }
+    protected void setNbmineligne(int nb){
+        this.nbcaseligne=nb;
+    }
+
+    protected void setNbminecolonne(int nb){
+        this.nbcasecolonne=nb;
+    }
+    protected  void setTabGrille(){
+        tabGrille = new int[nbcaseligne][nbcasecolonne];
+    }
+
+    public int getNbcaseligne() {
+        return nbcaseligne;
+    }
+
+    public int getNbcasecolonne() {
+        return nbcasecolonne;
+    }
+
+    public int[][] getTabGrille() {
+        return tabGrille;
     }
 }
