9621c84dfa12a103f6812d721c09b4bafc80d3d4
SamuelC <samuix21700@gmail.com>
2018-05-17 14:17:55 +0200

    Base d'appli et de model

--- src/Appli.java
+++ src/Appli.java
@@ -2,18 +2,8 @@ import java.io.IOException;
 
 public class Appli {
     public static void main(String[] args) {
-
-        javax.swing.SwingUtilities.invokeLater( new Runnable() {
-            public void run() {
-                Model model = null;
-                try {
-                    model = new Model();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                ControlGroup control = new ControlGroup(model);
-            }
-        });
+        Model model = new Model();
+        Fenetre fenetre = new Fenetre(model);
     }
 
 }
