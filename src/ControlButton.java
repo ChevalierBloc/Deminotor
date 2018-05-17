31f4d43759fedb4e0ef0e98759694cd7db8d034a
Cheimooh <maeva.somny@edu.univ-fcomte.fr>
2018-05-17 15:26:15 +0200

    commit fenetre

--- src/ControlButton.java
+++ src/ControlButton.java
@@ -1,2 +1,87 @@
-public class ControlButton {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ControlButton implements ActionListener {
+
+    Fenetre f;
+
+    ModelIntermediaire model;
+
+    public ControlButton(ModelIntermediaire model, Fenetre f) {
+        this.f = f;
+        this.model = model;
+        f.setControlBouton(this);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        JButton source = (JButton) e.getSource();
+
+        if (source == f.getbJeu()) {
+            f.changerVue(2);
+        }
+
+        else if (source == f.getbOption()) {
+            f.changerVue(3);
+        }
+
+        else if (source == f.getbQuitter()) {
+            System.exit(0);
+        }
+
+        else if(source == f.getbRetourJouer()){
+            f.changerVue(1);
+        }
+
+        else if(source == f.getbNorma()){
+            model.estTor = false;
+            f.changerVue(4);
+        }
+
+        else if(source == f.getbTor()){
+            model.estTor = true;
+            f.changerVue(4);
+        }
+
+        else if(source == f.getbPerso()){
+            f.changerVue(6);
+        }
+
+        else if(source == f.getbFacile()){
+            model.diffeculte = 1;
+            f.actualiser();
+            f.changerVue(5);
+        }
+
+        else if(source == f.getbMoyen()){
+            model.diffeculte = 2;
+            f.actualiser();
+            f.changerVue(5);
+        }
+
+        else if(source == f.getbDiffile()){
+            model.diffeculte = 3;
+            f.actualiser();
+            f.changerVue(5);
+        }
+
+        else if(source == f.getbRetourDifficulte()){
+            model.estTor = false;
+            f.changerVue(2);
+        }
+
+        else if(source == f.getbRetourGrille()){
+            model.diffeculte = 0;
+            f.changerVue(4);
+        }
+
+        else if(source == f.getbRetourPerso()){
+            f.changerVue(2);
+        }
+
+        else if(source == f.getbRetourOption()){
+            f.changerVue(1);
+        }
+
+    }
 }
--- src/Fenetre.java
+++ src/Fenetre.java
@@ -1,11 +1,10 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 
 public class Fenetre extends JFrame {
-
-    private Model model;
-
-    // Menu principal
+    private ModelIntermediaire model;
 
     JPanel panGeneral;
 
@@ -16,6 +15,7 @@ public class Fenetre extends JFrame {
     private JLabel imageDemineur;
     private JLabel nomDemineur;
 
+    private JLabel nomDemineurJouer;
     private JButton bNorma;
     private JButton bTor;
     private JButton bPerso;
@@ -27,8 +27,6 @@ public class Fenetre extends JFrame {
     private JLabel nomDemineurPerso;
     private JButton bRetourPerso;
 
-    // Menu difficulté
-
     private JButton bFacile;
     private JButton bMoyen;
     private JButton bDiffile;
@@ -36,69 +34,19 @@ public class Fenetre extends JFrame {
     private JLabel nomDemineurDifficulte;
     private JButton bRetourDifficulte;
 
+    private JLabel nomDemineurGrille;
+    private JButton bRetourGrille;
 
-
-    public Fenetre(Model model){
+    public Fenetre(ModelIntermediaire model) {
         this.model = model;
         initAttribut();
-        ajouterWidgetPrincipal();
-        setSize(500, 700);
+        ajouterWidgetVuePrincipal();
+        setSize(500,700);
         setTitle("Démineur");
         setLocationRelativeTo(null);
         setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  // Gestion de la fermeture
     }
-
-    public void initAttribut() {
-        panGeneral = new JPanel();
-
-        imageDemineur = new JLabel(new ImageIcon(new ImageIcon("images/demineur.jpg").getImage().getScaledInstance(50, 50, BufferedImage.SCALE_SMOOTH)));
-        nomDemineur = new JLabel("Démineur :)");
-        bJeu = new JButton("Jouer");
-        bOption = new JButton("Option");
-        bQuitter = new JButton("Quitter");
-
-        nomDemineur = new JLabel("Jouer ");
-        bNorma = new JButton("Jeu Normal");
-        bTor = new JButton("Jeu TOR");
-        bPerso = new JButton("Jeu Personnaliser");
-        bRetourJouer = new JButton("Retour");
-
-        nomDemineurOption = new JLabel("Option");
-        bRetourOption = new JButton("Retour");
-
-        nomDemineurPerso = new JLabel("Personnaliser");
-        bRetourPerso = new JButton("Retour");
-
-        nomDemineurDifficulte = new JLabel("Choississez la Difficulté");
-        bFacile = new JButton("Facile");
-        bMoyen = new JButton("Moyen");
-        bDiffile = new JButton("Difficile");
-        bRetourDifficulte = new JButton("Retour");
-    }
-    public void ajouterWidgetPrincipal() {
-        JPanel panDemineur = new JPanel();
-        panDemineur.setLayout(new BoxLayout(panDemineur, BoxLayout.X_AXIS));
-        panDemineur.add(imageDemineur);
-        panDemineur.add(nomDemineur);
-
-        JPanel panel = new JPanel();
-        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-        panel.add(panDemineur);
-        panel.add(bJeu);
-        panel.add(bOption);
-        panel.add(bQuitter);
-
-        panGeneral.add(panel);
-
-        setContentPane(panGeneral);
-    }
-
-    public void ajouterWidgetDifficulte(){
-
-
-    }
-
 
     public JButton getbJeu() {
         return bJeu;
@@ -110,14 +58,6 @@ public class Fenetre extends JFrame {
 
     public JButton getbQuitter() {
         return bQuitter;
-    }
-
-    public JLabel getImageDemineur() {
-        return imageDemineur;
-    }
-
-    public JLabel getNomDemineur() {
-        return nomDemineur;
     }
 
     public JButton getbNorma() {
@@ -136,16 +76,8 @@ public class Fenetre extends JFrame {
         return bRetourJouer;
     }
 
-    public JLabel getNomDemineurOption() {
-        return nomDemineurOption;
-    }
-
     public JButton getbRetourOption() {
         return bRetourOption;
-    }
-
-    public JLabel getNomDemineurPerso() {
-        return nomDemineurPerso;
     }
 
     public JButton getbRetourPerso() {
@@ -164,11 +96,183 @@ public class Fenetre extends JFrame {
         return bDiffile;
     }
 
-    public JLabel getNomDemineurDifficulte() {
-        return nomDemineurDifficulte;
-    }
-
     public JButton getbRetourDifficulte() {
         return bRetourDifficulte;
+    }
+
+    public JButton getbRetourGrille() {
+        return bRetourGrille;
+    }
+
+    public void setControlBouton(ActionListener actionListener){
+        bJeu.addActionListener(actionListener);
+        bOption.addActionListener(actionListener);
+        bQuitter.addActionListener(actionListener);
+
+        bNorma.addActionListener(actionListener);
+        bTor.addActionListener(actionListener);
+        bPerso.addActionListener(actionListener);
+        bRetourJouer.addActionListener(actionListener);
+
+        bRetourOption.addActionListener(actionListener);
+
+        bRetourPerso.addActionListener(actionListener);
+
+        bFacile.addActionListener(actionListener);
+        bMoyen.addActionListener(actionListener);
+        bDiffile.addActionListener(actionListener);
+        bRetourDifficulte.addActionListener(actionListener);
+
+        bRetourGrille.addActionListener(actionListener);
+    }
+
+    public void initAttribut(){
+        panGeneral = new JPanel();
+
+        imageDemineur = new JLabel(new ImageIcon(new ImageIcon("images/demineur.jpg").getImage().getScaledInstance(50, 50, BufferedImage.SCALE_SMOOTH)));
+        nomDemineur = new JLabel("Démineur :)");
+        bJeu = new JButton("Jouer");
+        bOption = new JButton("Option");
+        bQuitter = new JButton("Quitter");
+
+        nomDemineurJouer = new JLabel("Jouer ");
+        bNorma = new JButton("Jeu Normal");
+        bTor = new JButton("Jeu TOR");
+        bPerso = new JButton("Jeu Personnaliser");
+        bRetourJouer = new JButton("Retour");
+
+        nomDemineurOption = new JLabel("Option");
+        bRetourOption = new JButton("Retour");
+
+        nomDemineurPerso = new JLabel("Personnaliser");
+        bRetourPerso = new JButton("Retour");
+
+        nomDemineurDifficulte = new JLabel("Choississez la Difficulté");
+        bFacile = new JButton("Facile");
+        bMoyen = new JButton("Moyen");
+        bDiffile = new JButton("Difficile");
+        bRetourDifficulte = new JButton("Retour");
+
+        if(model.estTor)
+            nomDemineurGrille = new JLabel("Grille de difficulte " + model.diffeculte + " en TOR");
+        else
+            nomDemineurGrille = new JLabel("Grille de difficulte " + model.diffeculte + " en normal");
+        bRetourGrille = new JButton("Retour");
+    }
+
+    public void ajouterWidgetVuePrincipal(){
+        JPanel panDemineur = new JPanel();
+        panDemineur.setLayout(new BoxLayout(panDemineur, BoxLayout.X_AXIS));
+        //panDemineur.add(imageDemineur);
+        panDemineur.add(nomDemineur);
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        panel.add(panDemineur);
+        panel.add(bJeu);
+        panel.add(bOption);
+        panel.add(bQuitter);
+
+        panGeneral.add(panel);
+
+        setContentPane(panGeneral);
+    }
+
+    public void ajouterWidgetVueJouer(){
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        panel.add(nomDemineurJouer);
+        panel.add(bNorma);
+        panel.add(bTor);
+        panel.add(bPerso);
+        panel.add(bRetourJouer);
+
+        panGeneral.add(panel);
+
+        setContentPane(panGeneral);
+    }
+
+    public void ajouterWidgetVueOption(){
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
+        panel.add(nomDemineurOption);
+        panel.add(bRetourOption);
+
+        panGeneral.add(panel);
+
+        setContentPane(panGeneral);
+    }
+
+    public void ajouterWidgetVueDifficulte(){
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        panel.add(nomDemineurDifficulte);
+        panel.add(bFacile);
+        panel.add(bMoyen);
+        panel.add(bDiffile);
+        panel.add(bRetourDifficulte);
+
+        panGeneral.add(panel);
+
+        setContentPane(panGeneral);
+    }
+
+    public void ajouterWidgetVueGrille(){
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        panel.add(nomDemineurGrille);
+        panel.add(bRetourGrille);
+
+        panGeneral.add(panel);
+
+        setContentPane(panGeneral);
+    }
+
+
+    public void ajouterWidgetVuePerso(){
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
+        panel.add(nomDemineurPerso);
+        panel.add(bRetourPerso);
+
+        panGeneral.add(panel);
+
+        setContentPane(panGeneral);
+    }
+
+    public void changerVue(int vue){
+        switch(vue){
+            case 1:
+                panGeneral.remove(0);
+                ajouterWidgetVuePrincipal();
+                break;
+            case 2:
+                panGeneral.remove(0);
+                ajouterWidgetVueJouer();
+                break;
+            case 3:
+                panGeneral.remove(0);
+                ajouterWidgetVueOption();
+                break;
+            case 4:
+                panGeneral.remove(0);
+                ajouterWidgetVueDifficulte();
+                break;
+            case 5:
+                panGeneral.remove(0);
+                ajouterWidgetVueGrille();
+                break;
+            case 6:
+                panGeneral.remove(0);
+                ajouterWidgetVuePerso();
+                break;
+        }
+    }
+
+    public void actualiser(){
+        if(model.estTor)
+            nomDemineurGrille.setText("Grille de difficulte " + model.diffeculte + " en TOR");
+        else
+            nomDemineurGrille.setText("Grille de difficulte " + model.diffeculte + " en normal");
     }
 }
