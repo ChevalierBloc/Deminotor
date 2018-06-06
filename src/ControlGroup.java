8a9ea9c1a1399f7c9a3553e82de4091be61e699b
Tucduk <tugdual.girard@hotmail.fr>
2018-06-06 16:46:45 +0200

    ameliorer affichage partie personnaliser

--- src/ControlGroup.java
+++ src/ControlGroup.java
@@ -2,12 +2,15 @@ public class ControlGroup {
 
     private Model model ;
     private Fenetre vue ;
-    public ControlButtonJeuMenu cb ;
+    private ControlButtonJeuMenu cb ;
+    private ControlMenu cm;
 
     public ControlGroup(Model model){
         this.model = model ;
         vue = new Fenetre(model);
         cb = new ControlButtonJeuMenu(model,vue);
+        cm = new ControlMenu(model,vue);
+
     }
 }
 
---
+++ src/ControlMenu.java
@@ -0,0 +1,16 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ControlMenu implements ActionListener {
+    Fenetre f;
+    Model model;
+
+    public ControlMenu(Model model,Fenetre f) {
+        this.f = f;
+        this.model = model;
+    }
+
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
