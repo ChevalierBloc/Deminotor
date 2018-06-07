957b83626acc24d32df43902c5d879d60d1f01b8
LiMaO90 <lucasguerrero741@gmail.com>
2018-06-07 17:09:12 +0200

    ajout de la classe Time pour le temps

---
+++ src/Time.java
@@ -0,0 +1,30 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Time {
+
+    private Timer timer;
+    private Model model;
+    private Fenetre fenetre;
+
+    public Time(Model model) {
+        this.model = model;
+        timer = new Timer(1000, new TraitementTimer());
+    }
+
+    private class TraitementTimer implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            model.setScore(model.getScore()+1);
+            fenetre.actualiser();
+        }
+    }
+
+    public void start(){
+        timer.start();
+    }
+
+    public void stop(){
+        timer.stop();
+    }
+}
